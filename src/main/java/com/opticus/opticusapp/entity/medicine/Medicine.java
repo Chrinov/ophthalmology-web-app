96689c028c0717ea6961d263a8d75d6cb4e66503
Chrinov <48407575+Chrinov@users.noreply.github.com>
2020-02-06 01:22:48 +0100

    MAS changes

--- src/main/java/com/opticus/opticusapp/entity/medicine/Medicine.java
+++ src/main/java/com/opticus/opticusapp/entity/medicine/Medicine.java
@@ -41,6 +41,27 @@ public class Medicine {
 
     }
 
+    public Medicine(String name) {
+        this.name = name;
+    }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAdministeredMedicines(List<AdministeredMedicine> administeredMedicines) {
+        this.administeredMedicines = administeredMedicines;
+    }
 }
