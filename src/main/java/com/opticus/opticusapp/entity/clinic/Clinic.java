deb6c043aaaf08b7386d0baf860928532522d35e
Chrinov <48407575+Chrinov@users.noreply.github.com>
2020-02-07 10:30:02 +0100

    Update Clinic.java

--- src/main/java/com/opticus/opticusapp/entity/clinic/Clinic.java
+++ src/main/java/com/opticus/opticusapp/entity/clinic/Clinic.java
@@ -1,4 +1,21 @@
 package com.opticus.opticusapp.entity.clinic;
 
+@Entity
+@Table(name="clinic")
 public class Clinic {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "clinic_id")
+  private int id;
+  
+  @Column(name = "address")
+  private String address;
+  
+  @ManyToMany(mappedBy = "clinics")
+  private Set<Specialist> specialists = new HashSet<>();
+  
+  
+  
+  
 }
